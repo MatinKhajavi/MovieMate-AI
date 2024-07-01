6add9eb3c77cef58e2de8f58bd4919a2fce8c37e
MatinKhajavi <mat.khajavi@gmail.com>
2024-06-30 23:24:34 -0700

    Implementing DataLoader for ingesting data from CSV files.

---
+++ src/data_loader.py
@@ -0,0 +1,72 @@
+import pandas as pd
+from typing import List
+from llama_index.core import Document
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.core.schema import TextNode
+
+class DataLoader:
+    def __init__(self, input_path: str, text_column: str, metadata_columns: List[str], chunk_size: int = 512, chunk_overlap: int = 128):
+        """
+        Initializes the DataLoader with path to the CSV, the name of the text column,
+        and the list of metadata column names, all treated as private variables. It also initializes
+        the chunk size and overlap for the node parser.
+
+        :param input_path: str, the path to the CSV file containing the data.
+        :param text_column: str, the column name which contains the text for documents.
+        :param metadata_columns: list of str, a list of column names to be used as metadata.
+        :param chunk_size: int, the size of chunks for the node parser.
+        :param chunk_overlap: int, the overlap between chunks for the node parser.
+        """
+        self._input_path = input_path
+        self._text_column = text_column
+        self._metadata_columns = metadata_columns
+        self._chunk_size = chunk_size
+        self._chunk_overlap = chunk_overlap
+
+    def _load_data(self) -> pd.DataFrame:
+        """
+        Loads data from the specified CSV file path, intended for internal use only.
+
+        :return: DataFrame, loaded data from CSV.
+        """
+        return pd.read_csv(self._input_path)
+
+    def _create_documents(self, data: pd.DataFrame) -> List[Document]:
+        """
+        Creates a list of Document objects from a DataFrame, intended for internal use only.
+
+        :param data: DataFrame, the data from which to create Documents.
+        :return: list of Document objects.
+        """
+        documents = []
+        for _, row in data.iterrows():
+            metadata = {col: row[col] for col in self._metadata_columns}
+            document = Document(text=row[self._text_column], metadata=metadata)
+            documents.append(document)
+        return documents
+
+    def _create_nodes(self, documents: List[Document]) -> List[TextNode]:
+        """
+        Parses documents into nodes using SentenceSplitter with specified chunk size and overlap,
+        intended for internal use only.
+
+        :param documents: list of Document objects.
+        :return: list of TextNode objects derived from the documents.
+        """
+        parser = SentenceSplitter(chunk_size=self._chunk_size, chunk_overlap=self._chunk_overlap)
+        nodes = parser.get_nodes_from_documents(documents)
+        return nodes
+
+    def ingest_data(self) -> List[TextNode]:
+        """
+        Processes the CSV data by loading it, creating documents, and parsing those into nodes.
+        This is the public method intended for external use.
+
+        :return: list of TextNode objects.
+        """
+        data = self._load_data()
+        documents = self._create_documents(data)
+        nodes = self._create_nodes(documents)
+        return nodes
+
+
