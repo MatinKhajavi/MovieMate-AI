a7bf108eddabe45070af9da5d65a3faa4521207c
MatinKhajavi <mat.khajavi@gmail.com>
2024-09-02 13:57:31 -0700

    Update the setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name="moviemateAI",
     version="0.1",
     packages=find_packages(),
+    license="MIT",
+    author="Matin Khajavi",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
